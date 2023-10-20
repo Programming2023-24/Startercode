ec5bbceed049d412c8a4a0b0d62bfcf74805610f
sverghes <94053918+sverghes@users.noreply.github.com>
2023-10-20 11:49:52 +0100

    Create MobilePlan.cpp

---
+++ MobilePlan.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include<string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	/*
+	* Declare Plan A Variables
+	* Example: 
+	float planACost = 2999;
+	float planAMins = ;
+	float planAData = ;
+	float planAaddMinsCost = ;
+	float planAaddDataCost = ;
+	*/
+
+	/*
+	* Declare Plan B Variables
+	* Example:
+	float planBCost = ;
+	float planBMins = ;
+	float planBData = ;
+	float planBaddMinsCost = ;
+	float planBaddDataCost = ;
+	*/
+	
+	/*
+	* Declare variables for Plan C
+	*/
+	
+	/*
+	* Declare variables for 
+	* Customer Name
+	* Minutes used
+	* Data used
+	* the Plan type
+	*/
+
+	/*
+	* Write cout, cin and getline() statements to receive all the input:
+	* cout << "enter name " << endl;
+	* cout << "Enter plan (A,B,C)" << endl;
+	* cout << "Enter minutes used " << endl;
+	* cout << "Enter data used (in Gb) " << endl;
+	*/
+	
+	/*
+	* Workout Plan A Cost.
+	* Pseudocode:
+	* ExtraMinutes = MinutesUsed - TotalPlanAMinutes.
+	* If 
+		there are ExtraMinutes 
+	* then 
+		the cost of ExtraMinutes multiplied with the ExtraMinutes is added to The TotalPlanACost
+	* ExtraData = DataUsed - TotalPlanAData
+	* If
+	*	there is ExtraData
+	* then
+	*	the cost of ExtraData multiplied with the ExtraData is added to The TotalPlanACost
+	*/
+
+	/*
+	* Workout Plan B Cost.
+	*/
+
+	/*
+	* Print the Bill by displaying the Plan and the Total Cost.
+	* 
+	Display how much money package A customers would save if they purchased package B or C, 
+	and how much money package B customers would save if they purchased package C
+	* 
+	* This should be Easy, because you have already calculated the cost for all the Plans.
+	*/
+
+	return 0;
+}
