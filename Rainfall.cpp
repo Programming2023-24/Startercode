debae6d11956303768e3cd19e2facd70a6fb8564
sverghes <94053918+sverghes@users.noreply.github.com>
2023-10-27 12:27:12 +0100

    Create Rainfall.cpp

---
+++ Rainfall.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*
+	* Declare variables for reading start-month and end-month
+	* Declare variable for storing filename (optional)
+	* Declare the variable for fstream
+	* Declare variables for rainfall and to count the number of rainfall that is recorded
+	*/
+
+	/*
+	* 1. Read Filename
+	* 2. Open the file
+	* 3. Read the start-month and end-month
+	* 4. Use a loop to read the recorded rainfall
+	* 5. Find the sum of recorded rainfall
+	* 6. Find the average by dividing the sum with the total number of recorded rainfall
+	* 7. Print the value
+	*/
+
+	return 0;
+}
